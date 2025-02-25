064d5ce34f8e43fabe03a0619dc9ecaaa16106b6
Ticia D <84279128+TiciaD@users.noreply.github.com>
2025-02-25 15:20:13 -0500

    init vite-react project

---
+++ dice-mice/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+// import { User } from './models/user.model'
+
+function App() {
+  // const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
