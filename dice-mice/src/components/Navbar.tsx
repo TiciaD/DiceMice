5c1daa78d04c103fdaa3b79ace05864afe2d9dfd
Ticia D <84279128+TiciaD@users.noreply.github.com>
2025-03-06 21:39:33 -0500

    added logging

--- dice-mice/src/components/Navbar.tsx
+++ dice-mice/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button, Box, IconButton, Menu, MenuItem, Avatar, Tooltip } from "@mui/material";
 import { useUser } from "@/context/UserDataProvider";
@@ -10,6 +10,12 @@ const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   // const [anchorElRulesNav, setAnchorElRulesNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  useEffect(() => {
+    console.log("current env", import.meta.env.VITE_DISCORD_REDIRECT_URI)
+
+  }, [])
+
 
   const navLinks = NavigationLinks
 
